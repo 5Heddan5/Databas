f0b48433d49e9d79ae5d43749fe58d48272185c8
Hedvig <carlevag@gmail.com>
2024-11-18 14:29:24 +0100

    lägg till och ta bort

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const blogPost = [
+let blogPost = [
     {
         id: '1',
         content: "Nulla consectetur id mollit enim est amet in culpa enim laborum enim do elit deserunt. Et sint excepteur Lorem ut. Do nisi qui ad proident velit. Et dolor cillum quis pariatur id quis. Duis exercitation ipsum laboris veniam esse pariatur tempor ex pariatur reprehenderit. Nisi nostrud deserunt eu culpa veniam nisi adipisicing eu.",
--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require("express");
 
 // This is how you import stuff from other files. "./" means that we are looking for something inside the same folder as this file (index.js)
-const { blogPost } = require("./data.js");
+let { blogPost } = require("./data.js");
 
 // Creates the server by invoking the express function and assigning it to the app
 const app = express();
+
+app.use(express.json());
 
 // Endpoint for getting all the blog post
 app.get("/blog-posts", (req, res) => {
@@ -36,6 +38,65 @@ app.get("/blog-posts/:id", (req, res) => {
   return res
     .status(404)
     .json({ message: "The blog with that id was not found" });
+});
+
+// Endpoint for creating new blog post and add it to the blogPost-array
+
+app.post("/blog-posts", (req, res) => {
+  // This request needs a body object that contains the information about the blog post that the server should create
+  const body = req.body;
+
+  // Eller if (!body)
+  if (body === undefined) {
+    return res.status(400).json({ message: "The body is missing." });
+  }
+
+  const content = body.content;
+  const newId = blogPost.length + 1;
+
+  const newBlogPost = {
+    id: newId,
+    content,
+  };
+
+  blogPost.unshift(newBlogPost);
+  return res.status(201).json({ message: "The new blogPost was created" });
+});
+
+app.put("/blog-posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+
+  if (content === undefined) {
+    return res.status(400).json({ message: "The body is missing" });
+  }
+
+  const blog = blogPost.find((bp) => bp.id === id);
+
+  if (!blog) {
+    return res
+      .status(400)
+      .json({ message: "The blog with that id was not found" });
+  }
+
+  blog.content = content;
+
+  return res.json(blog);
+});
+
+app.delete("/blog-posts/:id", (req, res) => {
+  const { id } = req.params;
+  const blog = blogPost.find((bp) => bp.id === id);
+
+  if (!blog) {
+    return res
+      .status(400)
+      .json({ message: "The blog with that id was not found" });
+  }
+
+  blogPost = blogPost.filter((bp) => bp.id!==id)
+
+  return res.json({message: "The blogpost was remove successfully"});
 });
 
 app.listen(3000, () => {
