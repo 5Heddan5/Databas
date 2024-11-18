446ba691454b91697b5ae11a9ffcc713dc1c45bd
Hedvig <carlevag@gmail.com>
2024-11-18 12:42:13 +0100

    fixed

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 const blogPost = [
     {
-        id: 1,
+        id: '1',
         content: "Nulla consectetur id mollit enim est amet in culpa enim laborum enim do elit deserunt. Et sint excepteur Lorem ut. Do nisi qui ad proident velit. Et dolor cillum quis pariatur id quis. Duis exercitation ipsum laboris veniam esse pariatur tempor ex pariatur reprehenderit. Nisi nostrud deserunt eu culpa veniam nisi adipisicing eu.",
     },
 
     {
-        id: 2,
+        id: '2',
         content: "Nulla consectetur id mollit enim est amet in culpa enim laborum enim do elit deserunt. Et sint excepteur Lorem ut. Do nisi qui ad proident velit. Et dolor cillum quis pariatur id quis. Duis exercitation ipsum laboris veniam esse pariatur tempor ex pariatur reprehenderit. Nisi nostrud deserunt eu culpa veniam nisi adipisicing eu.",
     },
 
     {
-        id: 3,
+        id: '3',
     content: "Nulla consectetur id mollit enim est amet in culpa enim laborum enim do elit deserunt. Et sint excepteur Lorem ut. Do nisi qui ad proident velit. Et dolor cillum quis pariatur id quis. Duis exercitation ipsum laboris veniam esse pariatur tempor ex pariatur reprehenderit. Nisi nostrud deserunt eu culpa veniam nisi adipisicing eu.",
 },
 
 {
-    id: 4,
+    id: '4',
 content: "Nulla consectetur id mollit enim est amet in culpa enim laborum enim do elit deserunt. Et sint excepteur Lorem ut. Do nisi qui ad proident velit. Et dolor cillum quis pariatur id quis. Duis exercitation ipsum laboris veniam esse pariatur tempor ex pariatur reprehenderit. Nisi nostrud deserunt eu culpa veniam nisi adipisicing eu.",
 },
 
--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-//Import express in to our application
+// Import express in to our application
 const express = require("express");
 
-// This is how you import stuff from other files. "./" means that w are looking for something inside the same folder as this file (index.js)
-const blogPost = require("./data.js");
+// This is how you import stuff from other files. "./" means that we are looking for something inside the same folder as this file (index.js)
+const { blogPost } = require("./data.js");
 
-console.log(blogPost);
+// Creates the server by invoking the express function and assigning it to the app
+const app = express();
 
-const app = express ();
+// Endpoint for getting all the blog post
+app.get("/blog-posts", (req, res) => {
+  res.json(blogPost);
+});
+
+// Endpoint for getting a blogPost by id
+app.get("/blog-posts/:id", (req, res) => {
+  const params = req.params;
+  const id = params.id;
+
+  // Destructed alternative to get the id
+  // const {id} = req.params;
+
+  // Find is an array method, that loops through the array pn which it is invoked on.
+  const blog = blogPost.find((bp) => {
+    if (bp.id === id) {
+      return true;
+    }
+
+    return false;
+  });
+
+  if (blog) {
+    return res.json(blog);
+  }
+
+  return res
+    .status(404)
+    .json({ message: "The blog with that id was not found" });
+});
 
 app.listen(3000, () => {
-    console.log("Server is listening to port 3000");
+  console.log("Server is listening to port 3000");
 });
